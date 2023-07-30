8b8368dd379719b05eb73ffcec4e80cf5faa8c54
senasengul8 <44581008+senasengul8@users.noreply.github.com>
2023-07-30 17:39:54 +0300

    Update CustomerList.js

--- src/CustomerList.js
+++ src/CustomerList.js
@@ -1,15 +1,1 @@
-// CustomerList.js
-import React from 'react';
-import CustomerCard from './CustomerCard';
 
-const CustomerList = ({ customers }) => {
-  return (
-    <div className="customer-list">
-      {customers.map((customer) => (
-        <CustomerCard key={customer._id} customer={customer} />
-      ))}
-    </div>
-  );
-};
-
-export default CustomerList;
